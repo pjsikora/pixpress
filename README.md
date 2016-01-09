10a5324855cf2e8d1bb2f5c9586ffeb091fb417d
piotr <piotr@nitro-digital.com>
2016-01-09 23:37:34 +0100

    onoff library

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 #Raspberry PI - Quick how to
 
-Turnoff:
-
+Turnoff your PI:
 sudo shutdown -h now
+
+Copy your SD card image to your HDD:
+sudo dd if=/dev/disk7 of=raspberrypi.dmg
+
+Unmount your SD:
+diskutil unmountDisk /dev/disk7
+
+Format card:
+sudo newfs_msdos -F 16 /dev/disk7
+
+Restore your system:
+sudo dd if=~/Desktop/raspberrypi.dmg of=/dev/disk2
--- server.js
+++ server.js
@@ -4,19 +4,34 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var gpio = require('pi-gpio');
-// 
-// gpio.write(7, 1, function() {
-//   console.log("7th PIN fired");
-// });
+var express    = require('express'),       // call express
+    app        = express(),                // define our app using express
+    bodyParser = require('body-parser');
 
+// button is attaced to pin 17, led to 18
+var GPIO = require('onoff').Gpio,
+    led = new GPIO(18, 'out'),
+    button = new GPIO(17, 'in', 'both');
 
+// define the callback function
+function light(err, state) {
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+  // check the state of the button
+  // 1 == pressed, 0 == not pressed
+  if(state == 1) {
+    // turn LED on
+    led.writeSync(1);
+  } else {
+    // turn LED off
+    led.writeSync(0);
+  }
+
+}
+
+// pass the callback function to the
+// as the first argument to watch()
+button.watch(light);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -36,22 +51,13 @@ router.get('/soop', function(req, res) {
 });
 
 router.get('/enable', function(req, res) {
-  gpio.write(7, 1, function() {
-    console.log("7th PIN fired");
-  });
+  led.writeSync(1);
 });
 
 router.get('/disable', function(req, res) {
-  gpio.write(7, 0, function() {
-    console.log("7th PIN disabled");
-  });
-
+  led.writeSync(0);
 });
 
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/api', router);
 
 // START THE SERVER
